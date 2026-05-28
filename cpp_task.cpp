49ffbe3e1575b1601a47eecc0090c3e54b4f6bef
Sam Brodie <63566766+sam-brodie@users.noreply.github.com>
2026-05-28 20:45:19 +0200

    initial commit

---
+++ cpp_task.cpp
@@ -0,0 +1,77 @@
+#include <chrono>
+#include <atomic>
+#include <memory>
+#include <thread>
+#include <functional>
+#include <iostream>
+
+using namespace std::chrono_literals;
+
+void StartThread(
+    std::thread& thread,
+    std::atomic<bool>& running,
+    const std::function<bool(void)>& Process,
+    const std::chrono::seconds timeout)
+{
+    thread = std::thread(
+        [&] ()
+        {
+            auto start = std::chrono::high_resolution_clock::now();
+            while(running)
+            {
+                bool aborted = Process();
+
+                auto end = std::chrono::high_resolution_clock::now();
+                auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+                if (aborted || duration > timeout)
+                {
+                    running = false;
+                    break;
+                }
+            }
+        });
+}
+
+int main(int argc, char **argv)
+{
+    std::atomic<bool> my_running = true;
+    std::thread my_thread1, my_thread2;
+    int loop_counter1 = 0, loop_counter2 = 0;
+
+    // start actions in seprate threads and wait of them
+
+    StartThread(
+        my_thread1,
+        my_running, 
+        [&]()
+        {
+            // "some actions" simulated with waiting
+            std::this_thread::sleep_for(std::chrono::milliseconds(2000));
+            loop_counter1++;
+            return false;
+        },
+        10s); // loop timeout
+
+    StartThread(
+        my_thread2,
+        my_running, 
+        [&]()
+        {
+            // "some actions" simulated with waiting 
+            if (loop_counter2 < 5)
+            {
+                std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+                loop_counter2++;
+                return false;
+            }
+            return true;
+        },
+        10s); // loop timeout
+
+
+    my_thread1.join();
+    my_thread2.join();
+
+    // print execlution loop counters
+    std::cout << "C1: " << loop_counter1 << " C2: " << loop_counter2 << std::endl;
+}
---
+++ readme.md
@@ -0,0 +1,11 @@
+# Tasks
+1. Take a look at cpp_task.cpp
+What is the code doing, are there any issues, and if so how to fix them?
+
+2. Take a look at python_task.py
+The code tries to rotate a matrix 90° but it fails, fix it.
+
+3. Desing and Implement a C++ class that offers interface to:
+send emmediatly a measege over udp to specific ip&port
+send with a non blocking request a udp message to specific ip&port after X seconds' delay (where X could be any value between 1s and 255s)
+request periodic sending of a udp message to specific ip&port every  X seconds. (where X could be any value between 1s and 255s)
