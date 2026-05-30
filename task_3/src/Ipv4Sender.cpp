906be0caf165ae8a90de179c7bdccbbdf18a7436
Sam Brodie <63566766+sam-brodie@users.noreply.github.com>
2026-05-30 18:50:23 +0200

    Improve task 3. Now uses a single scheduler thread. Messages are inserted into an ordered queue and the scheduler sends them at the correct time.

--- task_3/src/Ipv4Sender.cpp
+++ task_3/src/Ipv4Sender.cpp
@@ -15,19 +15,17 @@ Ipv4Sender::Ipv4Sender(const size_t max_buf_size)
         throw std::runtime_error("Could not create socket");
     }
     running = true;
+    scheduling_thread = std::thread(&Ipv4Sender::scheduler_loop, this);
 }
 
 Ipv4Sender::~Ipv4Sender()
 {
     // join all threads, close socket, etc
     running = false;
-    for (auto& t : periodic_threads)
-        if (t.joinable())
-            t.join();
+    queue_cond_var.notify_one();
 
-    for (auto& t : delayed_send_threads)
-        if (t.joinable())
-            t.join();
+    if (scheduling_thread.joinable())
+        scheduling_thread.join();
 
     if (sockfd >= 0)
         close(sockfd);
@@ -35,11 +33,16 @@ Ipv4Sender::~Ipv4Sender()
 
 
 // send emmediatly a measege over udp to specific ip&port
-size_t Ipv4Sender::send_immediately_ipv4(std::shared_ptr<const std::vector<uint8_t>> databuf, const std::string& dest_ip, const uint16_t dest_port)
+ssize_t Ipv4Sender::send_immediately_ipv4(
+    std::shared_ptr<const std::vector<uint8_t>> databuf,
+    const std::string& dest_ip,
+    const uint16_t dest_port)
 {
-    if (databuf->size() > max_buf_size) {
+    if (!databuf)
+        return ERR_NULL_BUFFER;
+    if (databuf->size() > max_buf_size)
         return ERR_DATA_TOO_LONG;
-    }
+
     sockaddr_in addr{};
     addr.sin_family = AF_INET;
     addr.sin_port = htons(dest_port);
@@ -51,50 +54,132 @@ size_t Ipv4Sender::send_immediately_ipv4(std::shared_ptr<const std::vector<uint8
     }
 
     int flags = 0;
-    size_t sent = sendto(sockfd, databuf->data(), databuf->size(), flags,
+    ssize_t sent = sendto(sockfd, databuf->data(), databuf->size(), flags,
         reinterpret_cast<sockaddr*>(&addr), sizeof(addr));
 
     if (sent < 0)
     {
-        std::cerr << "Failed to send UDP packet\n";
-        return -1;
+        std::cerr << "Failed to send UDP packet" << std::endl;
+        return ERR_SOCKET_ERROR;
     }
 
     return sent;
 }
 
-
 // send with a non blocking request a udp message to specific ip&port after X seconds' delay (where X could be any value between 1s and 255s)
-int Ipv4Sender::send_after_delay_nonblocking_ipv4(std::shared_ptr<const std::vector<uint8_t>> databuf, const std::string dest_ip, const uint16_t dest_port, const uint8_t delay)
+int Ipv4Sender::send_after_delay_nonblocking_ipv4(
+    std::shared_ptr<const std::vector<uint8_t>> databuf,
+    const std::string dest_ip,
+    const uint16_t dest_port,
+    const uint8_t delay)
 {
-    // start a thread that will wait X seconds, send the data, then exit
-    delayed_send_threads.push_back(std::thread(
-        [this, databuf, dest_ip, dest_port, delay]()
-        {
-            std::this_thread::sleep_for(std::chrono::seconds(delay));
-            this->send_immediately_ipv4(databuf, dest_ip, dest_port);
-        }));
+    if (delay == 0)
+        return ERR_INVALID_PERIOD;
+    if (!databuf)
+        return ERR_NULL_BUFFER;
+
+    UdpMsg msg{
+            databuf,
+            dest_ip,
+            dest_port,
+            0, // no repeating
+            std::chrono::steady_clock::now() + std::chrono::seconds(delay)
+    };
+
+    add_to_schedule_queue(std::move(msg));
 
     return STATUS_OK;
 }
 
 //request periodic sending of a udp message to specific ip& port every  X seconds. (where X could be any value between 1s and 255s)
-int Ipv4Sender::send_periodic_nonblocking_ipv4(std::shared_ptr<const std::vector<uint8_t>> databuf, const std::string dest_ip, const uint16_t dest_port, const uint8_t period)
+int Ipv4Sender::send_periodic_nonblocking_ipv4(
+    std::shared_ptr<const std::vector<uint8_t>> databuf,
+    const std::string dest_ip,
+    const uint16_t dest_port,
+    const uint8_t period)
 {
     if (period == 0)
         return ERR_INVALID_PERIOD;
+    if (!databuf)
+        return ERR_NULL_BUFFER;
 
-    // start a thread that will wait X seconds, send the data, then repeat
-    periodic_threads.push_back(std::thread(
-        [this, databuf, dest_ip, dest_port, period]()
-        {
-            while (this->running)
-            {
-                this->send_after_delay_nonblocking_ipv4(databuf, dest_ip, dest_port, period);
-                std::this_thread::sleep_for(std::chrono::seconds(period));
-            }
+    UdpMsg msg{
+            databuf,
+            dest_ip,
+            dest_port,
+            period,
+            std::chrono::steady_clock::now() + std::chrono::seconds(period)
+    };
 
-        }));
+    add_to_schedule_queue(std::move(msg));
 
     return STATUS_OK;
+}
+
+
+void Ipv4Sender::add_to_schedule_queue(UdpMsg msg)
+{
+    {
+        std::lock_guard<std::mutex> lock(scheduled_msgs_mutex);
+        scheduled_msgs.push(std::move(msg)); //consider std::move()
+    }
+    queue_cond_var.notify_one();
+}
+
+void Ipv4Sender::scheduler_loop()
+{
+    std::unique_lock<std::mutex> lock(scheduled_msgs_mutex);
+
+    while (running)
+    {
+        // If there are no messages waiting then wait until there is one or running is set to false
+        if (scheduled_msgs.empty())
+        {
+            // Equivalent to
+            //    while (!running || !scheduled_msgs.empty())
+            //        wait(lock);
+            queue_cond_var.wait(lock, [&] {
+                return !running || !scheduled_msgs.empty();
+                });
+
+            if (!running)
+                return;
+        }
+
+
+        while (running && !scheduled_msgs.empty())
+        {
+            auto next_time = scheduled_msgs.top().trigger_time;
+
+            // wait until "next_time" or until this thread is notified
+            if (queue_cond_var.wait_until(lock, next_time) != std::cv_status::timeout)
+            {
+                // we get here if the thread is notified before the next message is ready to send
+                // this is because another message is added to the queue. It might be the new most
+                // urgent message so we continue the loop and re-take the time from top of the 
+                // queue in case it has changed.
+                if (!running)
+                    return;
+
+                continue;
+            }
+
+            // the wait_until waited until the "next_time" trigger time without the thread being
+            // notified (due to new messages being added to the queue)
+            // This means that it is now "next_time" and so we should send the message
+            UdpMsg msg = scheduled_msgs.top();
+            scheduled_msgs.pop();
+
+            lock.unlock();
+            send_immediately_ipv4(msg.databuf, msg.dest_ip, msg.dest_port);
+            lock.lock();
+
+            if (msg.repeat_interval_sec > 0)
+            {
+                msg.trigger_time += std::chrono::seconds(msg.repeat_interval_sec);
+                scheduled_msgs.push(std::move(msg));
+                queue_cond_var.notify_one();
+            }
+        } //while (running && !scheduled_msgs.empty())
+    } // while (running)
 }
--- task_3/src/Ipv4Sender.hpp
+++ task_3/src/Ipv4Sender.hpp
@@ -2,42 +2,78 @@
 #define INTERVIEW_UDP_TASK_HPP
 
 #include <cstdint>
-#include <iostream>
 #include <string>
 #include <memory>
 #include <chrono>
 #include <thread>
 #include <atomic>
 #include <vector>
-#include <cstdio>
-
-
+#include <queue>
+#include <mutex>
+#include <condition_variable>
+#include <stdexcept>
+#include <iostream>
 
 class Ipv4Sender {
 public:
     static const int ERR_DATA_TOO_LONG = -1;
     static const int ERR_INVALID_ADDRESS = -2;
     static const int ERR_INVALID_PERIOD = -3;
+    static const int ERR_NULL_BUFFER = -4;
+    static const int ERR_SOCKET_ERROR = -5;
     static const int STATUS_OK = 0;
 
     Ipv4Sender(const size_t max_buf_size);
     ~Ipv4Sender();
 
     // send emmediatly a measege over udp to specific ip&port
-    size_t send_immediately_ipv4(std::shared_ptr<const std::vector<uint8_t>> databuf, const std::string& dest_ip, const uint16_t dest_port);
+    ssize_t send_immediately_ipv4(
+        std::shared_ptr<const std::vector<uint8_t>> databuf,
+        const std::string& dest_ip, 
+        const uint16_t dest_port);
 
     // send with a non blocking request a udp message to specific ip&port after X seconds' delay (where X could be any value between 1s and 255s)
-    int send_after_delay_nonblocking_ipv4(std::shared_ptr<const std::vector<uint8_t>> databuf, const std::string dest_ip, const uint16_t dest_port, const uint8_t delay);
+    int send_after_delay_nonblocking_ipv4(
+        std::shared_ptr<const std::vector<uint8_t>> databuf,
+        const std::string dest_ip,
+        const uint16_t dest_port,
+        const uint8_t delay);
 
     //request periodic sending of a udp message to specific ip& port every  X seconds. (where X could be any value between 1s and 255s)
-    int send_periodic_nonblocking_ipv4(std::shared_ptr<const std::vector<uint8_t>> databuf, const std::string dest_ip, const uint16_t dest_port, const uint8_t period);
+    int send_periodic_nonblocking_ipv4(
+        std::shared_ptr<const std::vector<uint8_t>> databuf,
+        const std::string dest_ip,
+        const uint16_t dest_port,
+        const uint8_t period);
 
 private:
     int sockfd = -1;
     const size_t max_buf_size;
     std::atomic<bool> running = { false };
-    std::vector<std::thread> periodic_threads;
-    std::vector<std::thread> delayed_send_threads;
+    std::thread scheduling_thread;
+    std::mutex scheduled_msgs_mutex;
+    std::condition_variable queue_cond_var;
+
+    struct UdpMsg {
+        std::shared_ptr <const std::vector<uint8_t>> databuf; //data to send
+        std::string dest_ip;
+        uint16_t dest_port;
+        uint8_t repeat_interval_sec; // how many seconds to wait between repeating sends. 0 for no repeat
+        std::chrono::steady_clock::time_point trigger_time; // when should this message be sent
+    };
+
+    struct CompareMsgPriority {
+        bool operator()(const UdpMsg& a, const UdpMsg& b) const {
+            return a.trigger_time > b.trigger_time;
+        }// returns the most urgent message 
+    };
+
+    std::priority_queue<UdpMsg,
+        std::vector<UdpMsg>,
+        CompareMsgPriority> scheduled_msgs;
+
+    void add_to_schedule_queue(UdpMsg msg);
+    void scheduler_loop();
 };
 
 
